e5b9be0e7abb95068977eb0a58e815cfbbd5cebd
Faisal-Abdo <145707185+Faisal-Abdo@users.noreply.github.com>
2025-01-26 14:29:31 +0400

    Update Cross Join.sql

--- Cross Join.sql
+++ Cross Join.sql
@@ -3,12 +3,13 @@ use CompanyDB
 --Write a query to generate a list of all possible employee-project pairs.
 
 select name, project_name
-from Employees
-cross join Projects
+from Employees e
+cross join Projects p
+on e.department_id=p.department_id
 
 --Write a query to pair each department with every project in the database.
 
 select department_name, project_name
-from Department
-cross join Projects
-
+from Department d
+cross join Projects p
+on d.department_id=p.department_id
